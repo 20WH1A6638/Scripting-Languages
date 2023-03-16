b4f566dd2ab32246bc7d287a7a03b5314b2b40a1
20WH1A6638 <93579966+20WH1A6638@users.noreply.github.com>
2023-03-16 12:10:33 +0530

    Create exp7.rb

---
+++ exp7.rb
@@ -0,0 +1,18 @@
+print "Enter two numbers"
+x = gets.to_i
+y = gets.to_i
+if x == 20 or y == 20
+  print "True"
+else
+  sum = x+y
+  print sum
+end
+
+OUTPUT
+cse@admin-cse:~/Desktop$ ruby exp7.rb
+Enter two numbers 20 20
+
+Truecse@admin-cse:~/Desktop$ ruby exp7.rb
+Enter two numbers15
+15
+30cse@admin-cse:~/Desktop$ 
