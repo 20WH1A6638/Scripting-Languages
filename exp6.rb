728e9b0e849cf94b75884961e87c8c523e65c94c
20WH1A6638 <93579966+20WH1A6638@users.noreply.github.com>
2023-03-23 15:29:49 +0530

    Update and rename exp 6 to exp6.rb

--- exp6.rb
+++ exp6.rb
@@ -2,7 +2,9 @@ puts "Odd numbers between 9 to 1: "9.
 step 1, -2 do |x|     
     puts "#{x}" 
 end
+
 OUTPUT
+
 cse@admin-cse:~/Desktop$ vim exp6.rb
 cse@admin-cse:~/Desktop$ ruby exp6.rb
 Odd numbers between 9 to 1: 
